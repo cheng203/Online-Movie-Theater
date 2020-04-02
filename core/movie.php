6775ae6d284b3c664a88114766915f304a82cc91
momodidi119 <54601117+momodidi119@users.noreply.github.com>
2020-04-02 15:11:11 -0500

    Add files via upload

---
+++ core/movie.php
@@ -0,0 +1,45 @@
+<?php
+include_once('../core/MysqlConnector.php');
+
+Class UserSql{
+    private $conn;
+    function __construct(){
+        $this->conn = new MysqlConnector("localhost", "root", "root", "test");
+    }
+    function findMPrice($movieID,$ticketTypeID){
+        $sql = sprintf("SELECT price  from ticket_price where movie_id = '%s', ticket_type_id= '%s'", $movieID,$ticketTypeID);
+        $result = $this->conn->query_json($sql);
+        return $result;
+    }
+
+    function movieAdd($movieID,$name,$type_name,$release_date,$off_date,$director,$info,$duration,$rating,$url)
+    {
+     
+        $sql = sprintf("INSERT into movies(movie_id,name,type_name,release_date,off_date,director,info,duration,rating,url) values('%s', '%s', '%s', '%s', '%s','%s','%s','%s','%s','%s')", $movieID,$name,$type_name,$release_date,$off_date,$director,$info,$duration,$rating,$url);
+        return $this->conn->query($sql);
+   
+       
+    }
+    function movieDelete($movieID){
+    	$sql = sprintf("DELETE from movies WHERE movie_id = '%s'", $movieID);
+        return $this->conn->query($sql);
+
+    }
+    function movieModify($movieID,$name,$type_name,$release_date,$off_date,$director,$info,$duration,$rating,$url){
+
+       
+        $sql = sprintf("UPDATE movies SET name='%s',type_name='%s',release_date='%s',off_date='%s',director='%s',info='%s',duration='%s',rating='%s',url='%s' WHERE movie_id='%s'", $name,$type_name,$release_date,$off_date,$director,$info,$duration,$rating,$url, $movieID);
+        return $this->conn->query($sql);
+        
+    }
+
+
+
+
+}
+
+
+
+
+
+?>
