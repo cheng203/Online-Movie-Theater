3f7936039ab3a6082e47f36375e81a98101b861f
Minchao Zhu <zhuminchao@aliyun.com>
2020-04-02 21:53:55 -0500

    backend: modify MovieSql.php

--- core/MovieSql.php
+++ core/MovieSql.php
@@ -1,7 +1,7 @@
 <?php
 include_once('../core/MysqlConnector.php');
 
-Class UserSql{
+Class MovieSql{
     private $conn;
     function __construct(){
         $this->conn = new MysqlConnector("localhost", "root", "root", "test");
@@ -12,20 +12,20 @@ Class UserSql{
         return $result;
     }
 
-    function movieAdd($movieID,$name,$type_name,$release_date,$off_date,$director,$info,$duration,$rating,$url)
+    function addMovie($movieID,$name,$type_name,$release_date,$off_date,$director,$info,$duration,$rating,$url)
     {
      
-        $sql = sprintf("INSERT into movies(movie_id,name,type_name,release_date,off_date,director,info,duration,rating,url) values('%s', '%s', '%s', '%s', '%s','%s','%s','%s','%s','%s')", $movieID,$name,$type_name,$release_date,$off_date,$director,$info,$duration,$rating,$url);
+        $sql = sprintf("INSERT into movies(movie_id,name,type_name,release_date,off_date,director,info,duration,rating,url) values(null, '%s', '%s', '%s', '%s','%s','%s','%s','%s','%s')", $movieID,$name,$type_name,$release_date,$off_date,$director,$info,$duration,$rating,$url);
         return $this->conn->query($sql);
    
        
     }
-    function movieDelete($movieID){
+    function deleteMovie($movieID){
     	$sql = sprintf("DELETE from movies WHERE movie_id = '%s'", $movieID);
         return $this->conn->query($sql);
 
     }
-    function movieModify($movieID,$name,$type_name,$release_date,$off_date,$director,$info,$duration,$rating,$url){
+    function modifyMovie($movieID,$name,$type_name,$release_date,$off_date,$director,$info,$duration,$rating,$url){
 
        
         $sql = sprintf("UPDATE movies SET name='%s',type_name='%s',release_date='%s',off_date='%s',director='%s',info='%s',duration='%s',rating='%s',url='%s' WHERE movie_id='%s'", $name,$type_name,$release_date,$off_date,$director,$info,$duration,$rating,$url, $movieID);
