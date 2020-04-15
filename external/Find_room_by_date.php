4e3fd54296255dd4c7eee87f1a067a7fb758b237
momodidi119 <54601117+momodidi119@users.noreply.github.com>
2020-04-14 21:26:21 -0500

    About Room and Room_by_date

---
+++ external/Find_room_by_date.php
@@ -0,0 +1,22 @@
+<?php
+include_once('../core/RoomSql.php');
+
+session_start();
+//if(!isset($_SEESION["user_type"])||$_SEESION["user_type"]!=0){
+	//die("You have no permission.");
+//}
+
+$room_id = $_POST['room_id'];
+$date = $_POST['date'];
+$query = new RoomSql();
+$result=$query->findRoomByDate($room_id,$date);
+if($result!=""){
+	echo $result;
+}else{
+	echo "There is an ERROR";
+}
+
+
+
+
+?>
