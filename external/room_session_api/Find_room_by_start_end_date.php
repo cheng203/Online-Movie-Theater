a54a0523d3d533ae33e5ee69407a15d4f3c12be1
momodidi119 <54601117+momodidi119@users.noreply.github.com>
2020-04-19 21:03:40 -0500

    Add files via upload

--- external/room_session_api/Find_room_by_start_end_date.php
+++ external/room_session_api/Find_room_by_start_end_date.php
@@ -1,1 +1,31 @@
+<?php
+include_once('../../core/RoomSql.php');
 
+session_start();
+//if(!isset($_SESSION["user_type"])||$_SESSION["user_type"]!=0){
+	//die("You have no permission.");
+//}
+$test = $_POST['sendData'];
+$data=json_decode($test);
+$room_id=$data[0]->room_id;
+//echo $data[0]->movie_name;
+$start_date = $data[1]->start_date;
+//echo $start_date;
+$end_date = $data[2]->end_date;
+//echo $end_date;
+$query = new RoomSql();
+
+$result1= $query->findRoomByStartEndDate($room_id,$start_date,$end_date);
+
+
+if($data!=""){
+	echo $result1;
+}else{
+	echo "There is an ERROR";
+}
+
+
+
+
+
+?>
