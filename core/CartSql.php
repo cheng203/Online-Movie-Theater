00d170dc1689f49f6d0a1482ccc6b549ec1632d2
momodidi119 <54601117+momodidi119@users.noreply.github.com>
2020-04-24 00:27:22 -0500

    Add files via upload

--- core/CartSql.php
+++ core/CartSql.php
@@ -6,16 +6,69 @@ Class CartSql{
     function __construct(){
         $this->conn = new MysqlConnector("localhost", "root", "root", "test");
     }
-    /*function findMoviePriceByID($movieID,$ticketTypeID){
-        $sql = sprintf("SELECT price  from ticket_price where movie_id = '%s' and ticket_type_id= '%s'", $movieID,$ticketTypeID);
-        $result = $this->conn->query_json($sql);
-        return $result;
-    }*/
+    
     function GetUserinCarts($user_id){
          $sql = sprintf("SELECT user_id  from carts where user_id = '%s'", $user_id);
         $result = $this->conn->query_json($sql);
         return $result;
     }
+
+    function FindCartInfo($user_id){
+        $sql = sprintf("SELECT * from carts where user_id = '%s'", $user_id);
+        $result = $this->conn->query_json($sql);
+        return $result;
+    }
+    function FindCartGoodsInfo($user_id){
+         $sql = sprintf("SELECT * from cart_goods_detail where user_id = '%s'", $user_id);
+        $result = $this->conn->query_json($sql);
+        return $result;
+    }
+     function FindCartTicketInfo($user_id){
+         $sql = sprintf("SELECT * from cart_tickets_detail where user_id = '%s'", $user_id);
+        $result = $this->conn->query_json($sql);
+        return $result;
+    }
+
+
+    function ModifyCartAmount($user_id,$good_amount){
+        $sql=  sprintf("UPDATE carts SET total_amount=total_amount+'%s' WHERE user_id='%s'", $good_amount,$user_id);
+
+        return $this->conn->query($sql);
+    }
+    function ModifyCartSessionAndAmount($user_id,$ticket_num,$session_id,$ticket_amount){
+        $sql=  sprintf("UPDATE carts SET number_tickets=number_tickets+'%s', session_id='%s',total_amount=total_amount+'%s' WHERE user_id='%s'", $ticket_num,$session_id,$ticket_amount,$user_id);
+
+        return $this->conn->query($sql);
+    }
+    
+
+
+    function GetUserinCart_goods_detail($user_id,$goods_id){
+         $sql = sprintf("SELECT user_id  from cart_goods_detail where user_id = '%s' and goods_id='%s'", $user_id,$goods_id);
+        $result = $this->conn->query_json($sql);
+        return $result;
+    }
+    function GetUserinCart_tickets_detail($user_id,$ticket_type_id){
+         $sql = sprintf("SELECT user_id  from cart_tickets_detail where user_id = '%s' and ticket_type_id='%s'", $user_id,$ticket_type_id);
+        $result = $this->conn->query_json($sql);
+        return $result;
+    }
+//
+    function AddOneGoodsInCart($user_id,$goods_id){
+     
+        $sql=  sprintf("UPDATE cart_goods_detail SET quantity=quantity+1 WHERE user_id='%s' and goods_id='%s'", $user_id,$goods_id);
+
+        return $this->conn->query($sql);
+    }
+
+    function AddOneTicketInCart($user_id,$ticket_type_id){
+        
+        $sql=  sprintf("UPDATE cart_tickets_detail SET quantity=quantity+1 WHERE user_id='%s' and ticket_type_id='%s'", $user_id,$ticket_type_id);
+
+        return $this->conn->query($sql);
+    }
+
+
 
     function AddCart($user_id,$number_tickets,$session_id,$total_amount)
     {
@@ -27,6 +80,22 @@ Class CartSql{
        
       
     }
+
+    function AddOnlyGoodinCart($user_id,$total_amount)
+    {
+     
+        $sql = sprintf("INSERT into carts (user_id, total_amount) values ('%s','%s')",$user_id,$total_amount);
+          $result=$this->conn->query($sql);
+          
+          return $result;
+       
+      
+    }
+    
+    /*function DeleteCart($user_id){
+        $sql = sprintf("DELETE from carts WHERE user_id = '%s'", $user_id);
+        return $this->conn->query($sql);
+    }*/
 
     function ModifyCart($user_id,$number_tickets,$session_id,$total_amount)
     {
@@ -45,7 +114,11 @@ Class CartSql{
         return $result;
 
     }
-   
+    //
+   /*function DeleteCart_goods_detail($user_id){
+        $sql = sprintf("DELETE from cart_goods_detail WHERE user_id = '%s'", $user_id);
+        return $this->conn->query($sql);
+    }*/
 
     function Modify_cart_goods_detail($user_id,$goods_id,$quantity)
     {
@@ -66,6 +139,12 @@ Class CartSql{
         return $result;
 
     }
+    //
+
+     /*function DeleteCart_tickets_detail($user_id){
+        $sql = sprintf("DELETE from cart_tickets_detail WHERE user_id = '%s'", $user_id);
+        return $this->conn->query($sql);
+    }*/
 
 
     function Modify_cart_tickets_detail($user_id,$ticket_type_id,$price,$quantity)
