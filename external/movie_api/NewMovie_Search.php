22e67c611cf8d0f2b16353ef20e1dcfc19112b1f
momodidi119 <54601117+momodidi119@users.noreply.github.com>
2020-04-23 21:41:37 -0500

    Add files via upload

---
+++ external/movie_api/NewMovie_Search.php
@@ -0,0 +1,29 @@
+<?php
+include_once('../../core/MovieSql.php');
+
+session_start();
+
+
+$query = new MovieSql();
+$movie_result=json_decode($query->findNewestFiveMovie());
+
+for($i=0;$i<5;$i++){
+    $new_result=$movie_result[$i];
+    $movie_id=$movie_result[$i]->movie_id;
+    $image=json_decode($query->getMovieSearchImages($movie_id));
+
+    $new_result->url=$image[0]->image_name;
+
+    $arr[]=$new_result;        
+}
+$result=json_encode($arr);
+if($result!=""){
+	echo $result;
+}else{
+	echo "Nothing";
+}
+
+
+
+
+?>
